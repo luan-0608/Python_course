3eec7330ec24539c561542f617771c0769adf0e0
Tran Luan <luanminh420@gmail.com>
2024-03-16 17:30:58 +0700

    update

--- Buoi7/dictionary.py
+++ Buoi7/dictionary.py
@@ -5,6 +5,12 @@ student = {"name": "Bob", "age": 23, "grades": [45, 67, 90, 98, 99]}
 # print(json.dumps(student,indent=4))
 
 # value= student["name"]
-value = student.get("id", "0001")
+# value = student.get("id", "0001")
+# print(value)
 
-print(value)
+student["id"] = "SV001"
+student["name"] = "jack"
+print(student)
+
+student.update(id="SV001", gender="male")
+print(json.dumps(student, indent=4))
---
+++ sum_max_min_Join.py
@@ -0,0 +1,3 @@
+lst= ['4','2','3','1']
+s= '--'.join(map(str,lst))
+print(s)
